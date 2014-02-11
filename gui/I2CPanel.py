5f504c08bb5bb196d3b3ed5da75e668bfbb7e5c2
faball <f.ball@cern.ch>
2014-02-11 12:47:03 +0100

    fixed pickle

--- gui/I2CPanel.py
+++ gui/I2CPanel.py
@@ -29,11 +29,22 @@ class I2CPanel :
 	class saveStateListener :
 		def __init__(self, panel) :
 			self._saveStatePanel=panel	
-			self._saveStatePanel.echoSelection()
+			self._saveStatePanel.returnStatement.setText("File saved: " + self._saveStatePanel.saveFileName.getText())
 			
 		def onRemoteResponse(self, response, request_info):
 			pass
 			
+		def onRemoteError(self, code, message, request_info):
+			ErrorMessage( "Unable to contact server" )
+			
+	
+	class loadStateListener :
+		def __init__(self, panel) :
+			self._saveStatePanel=panel	
+			
+		def onRemoteResponse(self, response, request_info):
+			self._saveStatePanel.returnStatement.setText("File loaded: " + self._saveStatePanel.loadFileName.getText())
+
 		def onRemoteError(self, code, message, request_info):
 			ErrorMessage( "Unable to contact server" )
 	
@@ -129,7 +140,7 @@ class I2CPanel :
 		self.callSettings = VerticalPanel("Load/Save States")
 		self.callSettings.setBorderWidth(1)
 
-		self.callSettings.add(HTML("<center>Save/Load State</center>"))
+		self.callSettings.add(HTML("<center>Load/Save State</center>"))
 		
 		cbcListAndCallSettings=VerticalPanel()
 		cbcListAndCallSettings.add(self.cbcList)
@@ -139,10 +150,15 @@ class I2CPanel :
 		self.mainPanel.add(self.channelMasks)
 		self.mainPanel.add(self.channelTrims)
 		
+		self.mainControlRegNames = ["FrontEndControl","TriggerLatency","HitDetectSLVS","Ipre1","Ipre2","Ipsf","Ipa","Ipaos","Vpafb","Icomp","Vpc","Vplus","VCth","TestPulsePot","SelTestPulseDel&ChanGroup","MiscTestPulseCtrl&AnalogMux","TestPulseChargePumpCurrent","TestPulseChargeMirrCascodeVolt","CwdWindow&Coincid","MiscStubLogic"]
+		self.channelMaskRegNames = ["MaskChannelFrom008downto001","MaskChannelFrom016downto009","MaskChannelFrom024downto017","MaskChannelFrom032downto025","MaskChannelFrom040downto033","MaskChannelFrom048downto041","MaskChannelFrom056downto049","MaskChannelFrom064downto057","MaskChannelFrom072downto065","MaskChannelFrom080downto073","MaskChannelFrom088downto081","MaskChannelFrom096downto089","MaskChannelFrom104downto097","MaskChannelFrom112downto105","MaskChannelFrom120downto113","MaskChannelFrom128downto121","MaskChannelFrom136downto129","MaskChannelFrom144downto137","MaskChannelFrom152downto145","MaskChannelFrom160downto153","MaskChannelFrom168downto161","MaskChannelFrom176downto169","MaskChannelFrom184downto177","MaskChannelFrom192downto185","MaskChannelFrom200downto193","MaskChannelFrom208downto201","MaskChannelFrom216downto209","MaskChannelFrom224downto217","MaskChannelFrom232downto225","MaskChannelFrom240downto233","MaskChannelFrom248downto241","MaskChannelFrom254downto249"]
+		self.channelTrimRegNames = ["Channel001","Channel002","Channel003","Channel004","Channel005","Channel006","Channel007","Channel008","Channel009","Channel010","Channel011","Channel012","Channel013","Channel014","Channel015","Channel016","Channel017","Channel018","Channel019","Channel020","Channel021","Channel022","Channel023","Channel024","Channel025","Channel026","Channel027","Channel028","Channel029","Channel030","Channel031","Channel032","Channel033","Channel034","Channel035","Channel036","Channel037","Channel038","Channel039","Channel040","Channel041","Channel042","Channel043","Channel044","Channel045","Channel046","Channel047","Channel048","Channel049","Channel050","Channel051","Channel052","Channel053","Channel054","Channel055","Channel056","Channel057","Channel058","Channel059","Channel060","Channel061","Channel062","Channel063","Channel064","Channel065","Channel066","Channel067","Channel068","Channel069","Channel070","Channel071","Channel072","Channel073","Channel074","Channel075","Channel076","Channel077","Channel078","Channel079","Channel080","Channel081","Channel082","Channel083","Channel084","Channel085","Channel086","Channel087","Channel088","Channel089","Channel090","Channel091","Channel092","Channel093","Channel094","Channel095","Channel096","Channel097","Channel098","Channel099","Channel100","Channel101","Channel102","Channel103","Channel104","Channel105","Channel106","Channel107","Channel108","Channel109","Channel110","Channel111","Channel112","Channel113","Channel114","Channel115","Channel116","Channel117","Channel118","Channel119","Channel120","Channel121","Channel122","Channel123","Channel124","Channel125","Channel126","Channel127","Channel128","Channel129","Channel130","Channel131","Channel132","Channel133","Channel134","Channel135","Channel136","Channel137","Channel138","Channel139","Channel140","Channel141","Channel142","Channel143","Channel144","Channel145","Channel146","Channel147","Channel148","Channel149","Channel150","Channel151","Channel152","Channel153","Channel154","Channel155","Channel156","Channel157","Channel158","Channel159","Channel160","Channel161","Channel162","Channel163","Channel164","Channel165","Channel166","Channel167","Channel168","Channel169","Channel170","Channel171","Channel172","Channel173","Channel174","Channel175","Channel176","Channel177","Channel178","Channel179","Channel180","Channel181","Channel182","Channel183","Channel184","Channel185","Channel186","Channel187","Channel188","Channel189","Channel190","Channel191","Channel192","Channel193","Channel194","Channel195","Channel196","Channel197","Channel198","Channel199","Channel200","Channel201","Channel202","Channel203","Channel204","Channel205","Channel206","Channel207","Channel208","Channel209","Channel210","Channel211","Channel212","Channel213","Channel214","Channel215","Channel216","Channel217","Channel218","Channel219","Channel220","Channel221","Channel222","Channel223","Channel224","Channel225","Channel226","Channel227","Channel228","Channel229","Channel230","Channel231","Channel232","Channel233","Channel234","Channel235","Channel236","Channel237","Channel238","Channel239","Channel240","Channel241","Channel242","Channel243","Channel244","Channel245","Channel246","Channel247","Channel248","Channel249","Channel250","Channel251","Channel252","Channel253","Channel254","ChannelDummy"]
+		
+		
 		self.callSettings.add( self.createStatesPanel())
-		self.mainSettings.add( self.createRegisterPanel(["FrontEndControl","TriggerLatency","HitDetectSLVS","Ipre1","Ipre2","Ipsf","Ipa","Ipaos","Vpafb","Icomp","Vpc","Vplus","VCth","TestPulsePot","SelTestPulseDel&ChanGroup","MiscTestPulseCtrl&AnalogMux","TestPulseChargePumpCurrent","TestPulseChargeMirrCascodeVolt","CwdWindow&Coincid","MiscStubLogic"]) )
-		self.channelMasks.add( self.createRegisterPanel(["MaskChannelFrom008downto001","MaskChannelFrom016downto009","MaskChannelFrom024downto017","MaskChannelFrom032downto025","MaskChannelFrom040downto033","MaskChannelFrom048downto041","MaskChannelFrom056downto049","MaskChannelFrom064downto057","MaskChannelFrom072downto065","MaskChannelFrom080downto073","MaskChannelFrom088downto081","MaskChannelFrom096downto089","MaskChannelFrom104downto097","MaskChannelFrom112downto105","MaskChannelFrom120downto113","MaskChannelFrom128downto121","MaskChannelFrom136downto129","MaskChannelFrom144downto137","MaskChannelFrom152downto145","MaskChannelFrom160downto153","MaskChannelFrom168downto161","MaskChannelFrom176downto169","MaskChannelFrom184downto177","MaskChannelFrom192downto185","MaskChannelFrom200downto193","MaskChannelFrom208downto201","MaskChannelFrom216downto209","MaskChannelFrom224downto217","MaskChannelFrom232downto225","MaskChannelFrom240downto233","MaskChannelFrom248downto241","MaskChannelFrom254downto249"]) )
-		self.channelTrims.add( self.createRegisterPanel(["Channel001","Channel002","Channel003","Channel004","Channel005","Channel006","Channel007","Channel008","Channel009","Channel010","Channel011","Channel012","Channel013","Channel014","Channel015","Channel016","Channel017","Channel018","Channel019","Channel020","Channel021","Channel022","Channel023","Channel024","Channel025","Channel026","Channel027","Channel028","Channel029","Channel030","Channel031","Channel032","Channel033","Channel034","Channel035","Channel036","Channel037","Channel038","Channel039","Channel040","Channel041","Channel042","Channel043","Channel044","Channel045","Channel046","Channel047","Channel048","Channel049","Channel050","Channel051","Channel052","Channel053","Channel054","Channel055","Channel056","Channel057","Channel058","Channel059","Channel060","Channel061","Channel062","Channel063","Channel064","Channel065","Channel066","Channel067","Channel068","Channel069","Channel070","Channel071","Channel072","Channel073","Channel074","Channel075","Channel076","Channel077","Channel078","Channel079","Channel080","Channel081","Channel082","Channel083","Channel084","Channel085","Channel086","Channel087","Channel088","Channel089","Channel090","Channel091","Channel092","Channel093","Channel094","Channel095","Channel096","Channel097","Channel098","Channel099","Channel100","Channel101","Channel102","Channel103","Channel104","Channel105","Channel106","Channel107","Channel108","Channel109","Channel110","Channel111","Channel112","Channel113","Channel114","Channel115","Channel116","Channel117","Channel118","Channel119","Channel120","Channel121","Channel122","Channel123","Channel124","Channel125","Channel126","Channel127","Channel128","Channel129","Channel130","Channel131","Channel132","Channel133","Channel134","Channel135","Channel136","Channel137","Channel138","Channel139","Channel140","Channel141","Channel142","Channel143","Channel144","Channel145","Channel146","Channel147","Channel148","Channel149","Channel150","Channel151","Channel152","Channel153","Channel154","Channel155","Channel156","Channel157","Channel158","Channel159","Channel160","Channel161","Channel162","Channel163","Channel164","Channel165","Channel166","Channel167","Channel168","Channel169","Channel170","Channel171","Channel172","Channel173","Channel174","Channel175","Channel176","Channel177","Channel178","Channel179","Channel180","Channel181","Channel182","Channel183","Channel184","Channel185","Channel186","Channel187","Channel188","Channel189","Channel190","Channel191","Channel192","Channel193","Channel194","Channel195","Channel196","Channel197","Channel198","Channel199","Channel200","Channel201","Channel202","Channel203","Channel204","Channel205","Channel206","Channel207","Channel208","Channel209","Channel210","Channel211","Channel212","Channel213","Channel214","Channel215","Channel216","Channel217","Channel218","Channel219","Channel220","Channel221","Channel222","Channel223","Channel224","Channel225","Channel226","Channel227","Channel228","Channel229","Channel230","Channel231","Channel232","Channel233","Channel234","Channel235","Channel236","Channel237","Channel238","Channel239","Channel240","Channel241","Channel242","Channel243","Channel244","Channel245","Channel246","Channel247","Channel248","Channel249","Channel250","Channel251","Channel252","Channel253","Channel254","ChannelDummy"]) )
+		self.mainSettings.add( self.createRegisterPanel(self.mainControlRegNames) )
+		self.channelMasks.add( self.createRegisterPanel(self.channelMaskRegNames) )
+		self.channelTrims.add( self.createRegisterPanel(self.channelTrimRegNames) )
 		
 		self.mainSettings.add()
 		self.echo=Label()
@@ -153,43 +169,51 @@ class I2CPanel :
 	
 	def onChange( self, sender ) :
 		
-		
 		if sender == self.cbcList :
 			# Make a call to the RPC service to get the register values
-			self.rpcService.I2CRegisterValues( self.getTotalCBCs(), I2CPanel.ReadRegisterValueListener(self) )#fb - sends all CBCs
-					
+			self.rpcService.I2CRegisterValues( self.getTotalCBCs(), I2CPanel.ReadRegisterValueListener(self) )#fb - sends all CBCs to refresh all lists
+			self.load.setEnabled(True)
+			self.save.setEnabled(True)
+		elif sender == self.save:
+			msg=[]
+			msg.append(self.saveFileName.getText())
+			#save_msg = self.saveFileName.getText()
+			self.rpcService.saveStateValues(msg, I2CPanel.saveStateListener(self) ) 
+		elif sender == self.load:
+			msg = self.loadFileName.getText()
+			self.rpcService.loadStateValues(msg, I2CPanel.loadStateListener(self) )	
+			self.rpcService.I2CRegisterValues( self.getTotalCBCs(), I2CPanel.ReadRegisterValueListener(self) ) #refresh status
+				
 		# Sender must be a text box. Need to format the input.
-		else :
+		else : 
 			try:
-				# For some reason the '0x' at the start of the string is causing exceptions,
-				# even though it works fine with interactive python. I'll take it off anyway.
+			# For some reason the '0x' at the start of the string is causing exceptions,
+			# even though it works fine with interactive python. I'll take it off anyway.
 				string=sender.getText()
 				if( len(string)>=2 ) :
 					if string[0]=='0' and string[1]=='x' : string=string[2:]
-				value=int( string, 16 ) # convert as hex
-				# Cap values at 255
-				if value<0 : value=0
-				if value>255 : value=255
-				sender.setStyleAttribute( "background-color", "#FFFFFF" )
-				# Convert to the same format as everything else
+					value=int( string, 16 ) # convert as hex
+					# Cap values at 255
+					if value<0 : value=0
+					if value>255 : value=255
+					sender.setStyleAttribute( "background-color", "#FFFFFF" )
+			# Convert to the same format as everything else
 				sender.setText( "0x%02x"%value )
-				# Send the change to the RPC service
+			# Send the change to the RPC service
 				messageParameters = {}
 				for cbcName in self.getActiveCBCs() :
 					messageParameters[cbcName]={ sender.getTitle():value }
-				self.rpcService.setI2CRegisterValues( messageParameters, I2CPanel.DoNothingListener(self) )
-				self.rpcService.I2CRegisterValues( self.getTotalCBCs(), I2CPanel.ReadRegisterValueListener(self) )# Live refresh of the status box
-				
+					self.rpcService.setI2CRegisterValues( messageParameters, I2CPanel.DoNothingListener(self) )
+				self.rpcService.I2CRegisterValues( self.getTotalCBCs(), I2CPanel.ReadRegisterValueListener(self) )	
 				
 			except ValueError:
-				sender.setStyleAttribute( "background-color", "#FF3333" )
-				
-		#self.echoSelection()
+				sender.setStyleAttribute( "background-color", "#FF3333" )		
+		self.echoSelection()
 	
 	def echoSelection(self): #fb - a good "print screen" method
-		msg = " You pressed: "
-		for names in self.getCheckedStates():
-			msg += names
+		msg = " Buttons clicked: "
+		for names in self.getCheckedRegisters():
+			msg+=self.getCheckedRegisters()
 		self.echo.setText(msg)	
 			
 	def getList(self):
@@ -222,8 +246,19 @@ class I2CPanel :
 		for names in self.stateValueEntries:
 			if str(self.stateValueEntries[names].isChecked())=="True":
 				selectedStates.append(names)
-		selectedStates.append(self.fileName.getText())
+		selectedStates.append(self.saveFileName.getText())
 		return selectedStates
+	
+	def getCheckedRegisters(self):
+		selectedRegisters = []
+		for name in getCheckedStates:
+			if [name] == "Main Control":
+				selectedRegisters += self.mainControlRegNames
+			if name == "Channel Masks":
+				selectedRegisters += self.channelMaskRegNames
+			if name == "Channel Trims":
+				selectedRegisters += self.channelTrimRegNames	
+		return selectedRegisters
 		
 	def createRegisterPanel( self, registerNames ) :
 		"""
@@ -246,8 +281,8 @@ class I2CPanel :
 			newPanel.setCellHorizontalAlignment( statusBox, HasHorizontalAlignment.ALIGN_RIGHT )
 			newPanel.setCellWidth( statusBox, "20px" )
 			newPanel.setWidth("100%")
-			#newPanel.setStyleName("areaStyle")
-			#newPanel.setBorderWidth(5)
+			#newPanel.setStyleName("areaStyle");
+			#newPanel.setBorderWidth(5);
 			
 			newTextBox.setText("select chip...")
 			newTextBox.addChangeListener(self)
@@ -277,35 +312,33 @@ class I2CPanel :
 			self.stateValueEntries[name]=checkBox
 			registerSelection.add(checkBox)
 		
-		state = HorizontalPanel()
-		label = Label("FileName")
-		state.add(label)
-		fileTextBox = TextBox()
-		fileTextBox.setText("DatFile.dat")
-		fileTextBox.setWidth(80)
-		self.fileName = fileTextBox
-		state.add(fileTextBox)
+		#Tidy up 
+		loadPanel = HorizontalPanel()
+		loadFileTextBox = TextBox()
+		loadFileTextBox.setText("DatFile.txt")
+		loadFileTextBox.setWidth(80)
+		self.loadFileName = loadFileTextBox
+		loadPanel.add(loadFileTextBox)
+		self.load = Button("Load",getattr(self,"onChange")) #overrides default and sends it to onChange
+		loadPanel.add(self.load)
+		self.load.setEnabled(False)
 		
-		launch = HorizontalPanel()
-		self.save = Button("Save")
-		self.load = Button("Load")
-		self.save.addClickListener(self)
-		self.load.addClickListener(self)
-		launch.add(self.save)
-		launch.add(self.load)
+		savePanel = HorizontalPanel()
+		saveFileTextBox = TextBox()
+		saveFileTextBox.setText("DatFile.txt")
+		saveFileTextBox.setWidth(80)
+		self.saveFileName = saveFileTextBox
+		savePanel.add(saveFileTextBox)
+		self.save = Button("Save", getattr(self,"onChange"))
+		savePanel.add(self.save)
+		self.save.setEnabled(False)
+		
+		self.returnStatement = Label()
 		
 		vertPanel.add(registerSelection)
-		vertPanel.add(state)
-		vertPanel.add(launch)
+		vertPanel.add(loadPanel)
+		vertPanel.add(savePanel)
+		vertPanel.add(self.returnStatement)
 		
 		return vertPanel
-	
-	def onClick(self, sender) :
-		if sender == self.save:
-			self.rpcService.saveStateValues(None, I2CPanel.saveStateListener(self) ) 
-			pass
-		elif sender == self.load:
-			self.rpcService.loadStateValues(self.fileName, I2CPanel.DoNothingListener(self) )
-			
-
 
